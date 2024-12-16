4e1dfb7147b0012334794ce2bd9491eb1a3447c0
cos <cos@gmail.com>
2024-12-16 08:34:07 +0100

    Pierwsza_zmiana

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DodatkowyKopponentComponent } from './dodatkowy-kopponent/dodatkowy-kopponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DodatkowyKopponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dodatkowy-kopponent/dodatkowy-kopponent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dodatkowy-kopponent',
+  standalone: false,
+  
+  templateUrl: './dodatkowy-kopponent.component.html',
+  styleUrl: './dodatkowy-kopponent.component.css'
+})
+export class DodatkowyKopponentComponent {
+
+}
